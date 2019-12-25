86ef5511acaf9357755803b6a7820e4165e41c2c
neoistone <59179052+neoistone@users.noreply.github.com>
2019-12-25 19:07:00 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+$company = "Your company name";
+## whm login credentials
+$user = 'whm username'; // Reseller or owner username
+$pass = 'password'; // Reseller or owner password
+$host_domain ="whm-host-domain.com"; //without a http or https example : neoistone.com 
+$host = 'whm sub domain name.'.$host_domain;  // whm host subdomain for example // cpanel
+$user_account = ""; // leave it empty
+$user_pass = ""; // leave it empty
+#database credentials because storeing userame password
+$dbname = "db name"; // your database name
+$db_host="db host"; //leave it by defult
+$table = "db table"; // your table name //
+$db_username ="root"; // acess dxatabase username
+$db_password = "db password"; // database password
+
+#smtp credentials
+$Host = 'mail.smtp2go.com'; // smtp hostname
+$smtp_Username = 'mail infromation'; // smtp username  for auth
+$smtp_Password = 'password'; // smtp passwoord for auth
+$smtp_Port = "smtp port"; // smtp port
