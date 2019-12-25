5b580f8919700b6a9eed737765765a856dfab73f
neoistone <59179052+neoistone@users.noreply.github.com>
2019-12-25 19:09:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,27 +8,7 @@
 ## any issue this code repaly it hashtag #neoistone-whmacct or send mail with error screenshot to this email manikantasripadi@neoistone.com
 ## Enter your company name here because cpanel user send email  credentials on down dispaly company  like this Thankyou choosing your company name
 ## for example Thankyour choosing neoistone hosting like this
-$company = "Your company name";
-## whm login credentials
-$user = 'whm username'; // Reseller or owner username
-$pass = 'password'; // Reseller or owner password
-$host_domain ="whm-host-domain.com"; //without a http or https example : neoistone.com 
-$host = 'whm sub domain name.'.$host_domain;  // whm host subdomain for example // cpanel
-$user_account = ""; // leave it empty
-$user_pass = ""; // leave it empty
-#database credentials because storeing userame password
-$dbname = "db name"; // your database name
-$db_host="db host"; //leave it by defult
-$table = "db table"; // your table name //
-$db_username ="root"; // acess dxatabase username
-$db_password = "db password"; // database password
-
-#smtp credentials
-$Host = 'mail.smtp2go.com'; // smtp hostname
-$smtp_Username = 'mail infromation'; // smtp username  for auth
-$smtp_Password = 'password'; // smtp passwoord for auth
-$smtp_Port = "smtp port"; // smtp port
-
+include_once("config.php");
 echo '<!DOCTYPE HTML>';
 echo '<html>';
 echo "<head>";
